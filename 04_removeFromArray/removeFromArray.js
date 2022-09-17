f2bf1ff1062dc8275d977d738bef36052afd7279
dannykryan <dannykryan@gmail.com>
2022-09-17 16:57:44 +0100

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+removeFromArray = (array, unwanted, unwanted2, unwanted3, unwanted4) => {
+    const filtered = array.filter((number) => {
+       return number !== unwanted && number !== unwanted2 && number !== unwanted3 && number !== unwanted4
+    });
+  
+    return filtered;
+  };
 
-};
+
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
