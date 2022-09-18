1158ba83f815c46bf8c4ad6f665f9f79ca91a063
dannykryan <dannykryan@gmail.com>
2022-09-18 12:34:22 +0100

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b, range=[]) {
+  
+    if (a < 0 || b < 0) return 'ERROR';
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    if (typeof a == 'string' || typeof b == 'string') return 'ERROR';
+   
+    for (let i = Math.min(a,b); i<= Math.max(a,b); i++) {
+      range.push(i);
+    }
+    return range.reduce((a,b) => a+b);  
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
+
