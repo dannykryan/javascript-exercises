882d0d30168962604209522d96d43d5e445f9a59
dannykryan <dannykryan@gmail.com>
2022-09-18 14:51:57 +0100

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  var result = (( f - 32 ) / 1.8 )
+  var rounded = Math.round(result * 10) /10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  var result = ((c * 1.8) + 32 )
+  var rounded = Math.round(result * 10) /10;
+  return rounded;
 };
 
 // Do not edit below this line
