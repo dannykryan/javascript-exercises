bc50485d6657fbe6d1c7d3b60c9fb66ce425e024
Danny Ryan <dannykryan@gmail.com>
2023-08-30 14:08:31 +0100

    Completed Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  var punctuation = /[\.,?!"\s]/g;
 
+  var stringA = string.toLowerCase().replace(punctuation, "");
+
+  var stringB = stringA.split("").reverse().join("");
+
+  if (stringA == stringB) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
