f5adefa4d51702b7b9ad0ac2f1ed253c87192dc7
Danny Ryan <dannykryan@gmail.com>
2023-09-12 20:49:36 +0100

    passed tests 1 and 2 of 3 on exercise 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
 
+  let currentAge = 0;
+  let oldestPerson = {};
+
+  for (let i = 0; i < people.length; i++) {
+    const age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+    if (age > currentAge) {
+      if (!people[i].yearOfDeath) {
+        continue;
+      }
+      oldestPerson = people[i];
+      currentAge = age;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// # Exercise 12 - Find the Oldest
+
+// Given an array of objects representing people with a birth and death year, return the oldest person.
+
+// ## Hints
+// - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
+// - this can be done with a couple of chained array methods, or by using `reduce`.
+// - One of the tests checks for people with no death-date.. use JavaScript's Date function to get their age as of today.
